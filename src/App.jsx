ffeec146f3bed137cbc5d2cd3f3432decec4f04a
Deepak <deepakkumar750206@gmail.com>
2022-07-10 13:20:02 +0530

    add board of tictactoe

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Bord from "./components/Board";
+import Board from "./components/Board";
 import "./styles/root.scss"
 const App = () =>{
   
   return (
     <div className="app">
       <h1>Tic Tac Toe!</h1>
-      <Bord />
+      <Board />
     </div>
   )
 }
